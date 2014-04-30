758b1dd8f5e37487bf83a0e52f102c8b9752a491
mrpenguin616 <jew12004@byui.edu>
2014-04-30 14:45:36 -0600

    added by ryan,

---
+++ ryanpush.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="utf-8">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/trialothon/mods/head.php'; ?>
+        <title> <!-- enter page Title here --></title> 
+    </head> 
+    <body>
+
+        <header>
+            <h1> <!-- enter page header here --> </h1>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/triathlon/mods/headernav.php'; ?>
+        </header>
+
+    <main>
+        <section class="page">
+        <!-- enter page content here -->
+        
+        </section>
+    </main> 
+
+    <footer>
+        
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/triathlon/mods/footer.php'; ?>
+    </footer> 
+</body>
+</html>
